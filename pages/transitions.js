0bca3a3a79f0bcf54104f721ad06e58b01913f39
Emanuel Baldi <emanuel.baldi@mirror.it>
2022-03-31 11:28:28 +0200

    start transition

---
+++ pages/transitions.js
@@ -0,0 +1,13 @@
+const Transitions = () => {
+	return (
+		<div className="w-full min-h-screen h-1">
+			<div className="flex h-full flex-col justify-center">
+				<div className="flex justify-center">
+					<h1 className="font-bold text-8xl">HOLA</h1>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Transitions;
